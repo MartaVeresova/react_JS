ce764612d40707484727757fe69dded37f40e326
Marta <martaveresovaa@gmail.com>
2021-02-09 20:34:49 +0300

    16.React JS (props)

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src='https://whatsism.com/uploads/posts/2018-07/1530546770_rmk_vdjbx10.jpg' />
-            Post 1
-                <div>
-                    <span>like</span>
-                </div>
+            {props.message}
+            <div>
+                <span>like</span>
+            </div>
         </div>
     )
 }
