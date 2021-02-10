9d40ab1e53ae022c63e606d41807322b49b5806c
Marta <martaveresovaa@gmail.com>
2021-02-10 17:02:50 +0300

    18.React JS (Dialogs)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,8 +9,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message = 'Hi, how are you?' />
-                <Post message = "It's my first post" />
+                <Post message='Hi, how are you?' likesCount='14'/>
+                <Post message="It's my first post" likesCount='25'/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -3,10 +3,10 @@ import s from './Post.module.css';
 const Post = (props) => {
     return (
         <div className={s.item}>
-            <img src='https://whatsism.com/uploads/posts/2018-07/1530546770_rmk_vdjbx10.jpg' />
+            <img src='https://whatsism.com/uploads/posts/2018-07/1530546770_rmk_vdjbx10.jpg'/>
             {props.message}
             <div>
-                <span>like</span>
+                <span>like</span> {props.likesCount}
             </div>
         </div>
     )
