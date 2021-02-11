e4f5cc88cbb64335579f14fc3dc3f18be3c238cc
Marta <martaveresovaa@gmail.com>
2021-02-11 20:11:22 +0300

    20.React JS (NavLink to, NavLink activeClassName)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
+
+//{`{${s.item} ${s.active}}`}
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='./Profile'>Profile</a>
-            </div>
-            <div className={`{${s.item} ${s.active}}`}>
-                <a href='./Dialogs'>Messages</a>
+                <NavLink to='./profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href='./News'>News</a>
+                <NavLink to='./dialogs' activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href='./Music'>Music</a>
+                <NavLink to='./news' activeClassName={s.activeLink}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href='./Settings'>Settings</a>
+                <NavLink to='./music' activeClassName={s.activeLink}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='./settings' activeClassName={s.activeLink}>Settings</NavLink>
             </div>
         </nav>
     )
